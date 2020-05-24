ce2b4e2ce219d804fe2f4bfbf8d1e8cb49a0425d
YoungChulShin <go1323@gmail.com>
2020-05-24 22:02:01 +0900

    Ristrict input for POST /api/events

--- src/main/java/me/ycshin/demoinflearnrestapi/events/EventController.java
+++ src/main/java/me/ycshin/demoinflearnrestapi/events/EventController.java
@@ -1,6 +1,7 @@
 package me.ycshin.demoinflearnrestapi.events;
 
 import lombok.RequiredArgsConstructor;
+import org.modelmapper.ModelMapper;
 import org.springframework.hateoas.MediaTypes;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
@@ -19,11 +20,13 @@ public class EventController {
 
     private final EventRepository eventRepository;
 
-    @PostMapping
-    public ResponseEntity createEvent(@RequestBody Event event) {
+    private final ModelMapper modelMapper;
 
+    @PostMapping
+    public ResponseEntity createEvent(@RequestBody EventDto eventDto) {
+        Event event = modelMapper.map(eventDto, Event.class);
         Event newEvent = this.eventRepository.save(event);
         URI createdUri = linkTo(EventController.class).slash(newEvent.getId()).toUri();
-        return ResponseEntity.created(createdUri).body(event);
+        return ResponseEntity.created(createdUri).body(newEvent);
     }
 }
--- src/main/java/me/ycshin/demoinflearnrestapi/events/EventDto.java
+++ src/main/java/me/ycshin/demoinflearnrestapi/events/EventDto.java
@@ -2,16 +2,11 @@ package me.ycshin.demoinflearnrestapi.events;
 
 import lombok.*;
 
-import javax.persistence.*;
 import java.time.LocalDateTime;
 
-@Builder @NoArgsConstructor @AllArgsConstructor
-@Getter @Setter @EqualsAndHashCode(of = "id")
-@Entity
-public class Event {
+@Data @Builder @NoArgsConstructor @AllArgsConstructor
+public class EventDto {
 
-    @Id @GeneratedValue
-    private Integer id;
     private String name;
     private String description;
     private LocalDateTime beginEnrollmentDateTime;
@@ -22,8 +17,4 @@ public class Event {
     private int basePrice;
     private int maxPrice;
     private int limitOfEnrollment;
-    private boolean offLine;
-    private boolean free;
-    @Enumerated(EnumType.STRING)
-    private EventStatus eventStatus = EventStatus.DRAFT;
 }
